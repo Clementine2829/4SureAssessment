4284a90d236937a8339151332418da2bf1254802
Clementine <clementinemamogale@gmail.com>
2024-11-17 15:23:36 +0200

    Changed nav bar color, added 3 dots menu, added background image, also added back press on menu

--- app/src/main/java/com/clementine/weatherapp/MainActivity.kt
+++ app/src/main/java/com/clementine/weatherapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.clementine.weatherapp
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.clementine.weatherapp.view.fragments.ForecastDetailFragment
 import com.clementine.weatherapp.view.fragments.MapFragment
@@ -12,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        title = "Weather App"
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, MapFragment())
             .commit()
@@ -21,21 +28,90 @@ class MainActivity : AppCompatActivity() {
             when (menuItem.itemId) {
                 R.id.nav_map -> {
                     loadFragment(MapFragment())
-//                    loadFragment(ForecastDetailFragment())
                     true
                 }
+
                 R.id.nav_forecast -> {
                     loadFragment(ForecastDetailFragment())
                     true
                 }
+
                 else -> false
             }
         }
+
+        handleBackPress()
     }
 
     private fun loadFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, fragment)
             .commit()
+    }
+
+    private fun handleBackPress() {
+        var backPressedTime: Long = 0
+        val exitToastDuration = 2000
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (supportFragmentManager.backStackEntryCount > 0) {
+                    supportFragmentManager.popBackStack()
+                } else {
+                    if (backPressedTime + exitToastDuration > System.currentTimeMillis()) {
+                        finishAffinity()
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Press back again to exit",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    backPressedTime = System.currentTimeMillis()
+                }
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: android.view.Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: android.view.MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.about -> {
+                showAboutDialog()
+                true
+            }
+
+            R.id.close_app -> {
+                closeApp()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun closeApp() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Close App")
+        builder.setMessage("Are you sure you want to close this app?")
+        builder.setPositiveButton("Yes") { dialog, _ ->
+            run {
+                finishAffinity()
+                dialog.dismiss()
+            }
+        }
+        builder.setNegativeButton("No") { dialog, _ -> dialog.dismiss() }
+        builder.show()
+    }
+
+    private fun showAboutDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("About")
+        builder.setMessage("This is an example app to demonstrate a toolbar with an overflow menu.")
+        builder.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+        builder.show()
     }
 }
